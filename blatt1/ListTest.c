cfae54c118a9d935af73059a4c985b70b04f9b7e
MatthiasThien <matthias.thien@web.de>
2017-11-08 13:36:06 +0100

    Überprüfung auf richtige Eingabe eingefügt

--- blatt1/ListTest.c
+++ blatt1/ListTest.c
@@ -3,18 +3,22 @@
 #include <stdlib.h>
 #include "ListTest.h"
 
+// Knotentyp. Speichert einen Verweis auf den nächsten Knoten und einen int Value
 struct node {
 	struct node *next;
 	int val;
 };
 
+// Listentyp. Merkt sich den ersten und letzten Knoten
 struct list {
 	struct node *first;
 	struct node *last;
 };
 
+// Fügt einer gegebenen Liste einen neuen Knoten mit dem angegebenen Value hinzu.
+// Fügt am Ende an
 struct list * addNode(struct list *inList, int inValue)
-{
+{	
 	if (inList->first == NULL) {
 		struct node *NodeBuff;
 		NodeBuff = malloc(sizeof(struct node));
@@ -42,6 +46,7 @@ struct list * addNode(struct list *inList, int inValue)
 	}
 }
 
+// Gibt alle Values einer Liste aus
 int printList(struct list *inList) {
 	struct node *Counter;
 	Counter = inList->first;
@@ -55,7 +60,7 @@ int printList(struct list *inList) {
 
 }
 
-
+// Löscht eine Liste und gibt den Speicher wieder frei
 int clearList(struct list *inList) {
 	struct node *temp, *temp2;
 	temp = inList->first;
@@ -69,6 +74,7 @@ int clearList(struct list *inList) {
 
 }
 
+// Gibt die Anzahl der Elemente einer Liste zurück
 int getListLength(struct list *inList) {
 	struct node *temp;
 	temp = inList->first;
@@ -90,7 +96,7 @@ int getListLength(struct list *inList) {
 		printf("split else 1\n");
 		struct list *secHalf;
 		struct node *nFirst, *nLast;
-
+		
 		printf("split else 2\n");
 		nFirst = inList->first;
 		printf("split else 3\n");
@@ -119,30 +125,66 @@ int mergesort(struct list *inList) {
 	printf("mergesort 3\n");
 }*/
 
+// Testet ob der char eine einzelne Zahl ist oder nicht
+int isDigit(char toTest) {
+	if (toTest == '0' || toTest == '1' || toTest == '2' || toTest == '3' || toTest == '4' ||
+		toTest == '5' || toTest == '6' || toTest == '7' || toTest == '8' || toTest == '9') {
+			return 42;
+	} 
+	return 0;
+}
+
+// Testet ob der gegebene String eine ganze Zahl(inkl negativ) ist oder nicht
+int isNumber(char *input) {	
+	int i, isNumber;
+	isNumber = 42;
+	if (!isDigit(input[0]) && input[0] != '-') {
+		isNumber = 0;
+	}
+	for (i = 1; i < strlen(input); i++) {
+		if (!isDigit(input[i])) {
+			isNumber = 0;
+		}
+	}
+	return isNumber;	 
+}
+
 
 int main(int argc, char **argv)
-{
+{	
 	printf("\n");
 
 	int i, j;
 	//printf("%s\n", "main 1");
+
+	// Erstellen der Liste
 	struct list *ListBuff;
 	ListBuff = malloc(sizeof(struct list));
-
-	for (i = 1; i < argc; i++)
-	{
-		//printf("%s\n", "main for 1");
-		addNode(ListBuff, atoi(argv[i]));
-		//printf("%s\n", "main for 2");
+	
+	// Testet ob die Übergabeparameter den Vorschriften entsprechen
+	int areNumbers;
+	areNumbers = 42;
+	for (i = 1; i < argc; i++) {
+		if (!isNumber(argv[i])) {
+			areNumbers = 0;
+		}
 	}
-	printList(ListBuff);
 
-	mergesort(ListBuff);
+	// Eine Liste wird nur erstellt wenn die Übergabeparameter korrekt waren.
+	if (areNumbers) {
+		for (i = 1; i < argc; i++) {	
+			//printf("%s\n", "main for 1");
+			addNode(ListBuff, atoi(argv[i]));
+			//printf("%s\n", "main for 2");
+		}
+		printList(ListBuff);
 
-	clearList(ListBuff);
+		//mergesort(ListBuff);
 
-	printf("\nProgram finished!\n");
+		clearList(ListBuff);
+
+		printf("\nProgram finished!\n");
+	} else {
+		printf("Fehler: Bitte gebe nur positive oder negative ganze Zahlen an.\n");
+	}
 }
-
-// ToDo- Bei dem addNode in der for schleife der Main muss noch eine Abfrage,
-// ob der übergebene wert ein int ist
